cd0a1ed0c7491c9616e275693cec68c8997c635d
ubsefor <letzdropthebas@gmail.com>
2022-02-14 10:26:25 +0300

    Initial Commit

---
+++ Intel Stripper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Intel Stripper
+//
+//  Created by Alexander Makhov on 14.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Intel Stripper/Intel_StripperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Intel_StripperApp.swift
+//  Intel Stripper
+//
+//  Created by Alexander Makhov on 14.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Intel_StripperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
